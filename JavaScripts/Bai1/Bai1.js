046cbe8039d749a3b4ea8192e290fbfeb5e6fcfd
codecuatai <vtai2805@gmail.com>
2025-10-16 20:41:52 +0700

    javascript basic

---
+++ JavaScripts/Bai1/Bai1.js
@@ -0,0 +1,23 @@
+alert("Đây là thông báo");
+var x = prompt("Nhập vào tên của bạn");
+
+var nyName;
+let lastName;
+var diemToan = 10;
+const doiso = 199; //bắt buộc phải có đối số
+alert("Điểm toán của bạn là : " + diemToan);
+
+alert("Chèo mừng x đến với me: " + x);
+
+// Video 8th
+
+let soNguyen = 1;
+let soThuc = 3.14;
+
+let check = false;
+
+let diemtoan;
+
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+console.log(typeof check);
